645514877f116f61c5650b9bd0b638b9e52abd33
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2024-11-07 20:44:28 -0300

    att

--- app.js
+++ app.js
@@ -4,11 +4,11 @@
  */
 
 function quebrar() {
-    document.getElementById('lamp').src="img/broken.jpg"
     // reproduzindo um arquivo de áudio no JS
     // passo 1: copiar o arquivo de áudio para o projeto
     // passo 2: usar a classe áudio (biblioteca interna do JS)
     let som = new Audio()
     som.src="sound/glassbreaking.wav"
     som.play()
+    document.getElementById('lamp').src="img/broken.jpg"
 }
