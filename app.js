e9a706774a0d9716630c3e0f9f04cd0510e2a71d
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2024-11-07 22:16:27 -0300

    lamp func

--- app.js
+++ app.js
@@ -3,12 +3,32 @@
  * @author Claudio Silva
  */
 
+// variáveis de apoio da lógica
+let onoff = false // o interruptor inicia desligado
+let lampada = true // a lampada está normal
+
 function quebrar() {
-    // reproduzindo um arquivo de áudio no JS
-    // passo 1: copiar o arquivo de áudio para o projeto
-    // passo 2: usar a classe áudio (biblioteca interna do JS)
-    let som = new Audio()
-    som.src="sound/glassbreaking.wav"
-    som.play()
-    document.getElementById('lamp').src="img/broken.jpg"
+    if (lampada === true) {
+        // reproduzindo um arquivo de áudio no JS
+        // passo 1: copiar o arquivo de áudio para o projeto
+        // passo 2: usar a classe áudio (biblioteca interna do JS)
+        let som = new Audio()
+        som.src = "sound/glassbreaking.wav"
+        som.play()
+        document.getElementById('lamp').src = "img/broken.jpg"
+        // apoio à lógica para o JS identificar a lâmpada quebrada
+        lampada = false
+    }
+}
+
+function chave() {
+    if (onoff === false && lampada === true) {
+        document.getElementById('interruptor').src = "img/swon.png"
+        onoff = true // o JS agora sabe que a chave está ligada
+        document.getElementById('lamp').src = "img/on.jpg"
+    } else if (lampada === true) {
+        document.getElementById('interruptor').src = "img/swoff.png"
+        onoff = false
+        document.getElementById('lamp').src = "img/off.jpg"
+    } 
 }
